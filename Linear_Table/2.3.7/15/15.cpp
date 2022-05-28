06f934b1727d8d13dbd92ddf9f4e57766725f104
MVPYPC <56836181+MVPYPC@users.noreply.github.com>
2022-05-28 20:33:37 +0800

    2.3.7/15 finished!

--- Linear_Table/2.3.7/15/15.cpp
+++ Linear_Table/2.3.7/15/15.cpp
@@ -1,42 +1,35 @@
 #include "C:/Users/86072/Documents/Clanguage/Datastructure_408/Linear_Table/def.h"
-void merge_2ascList_common1List(LinkList& L, LinkList A, LinkList B){
-    L = (LinkList)malloc(sizeof(LinkList));
-    LNode* p = L;
+void merge_2ascList_2A(LinkList& A, LinkList B){
     LNode* pA;
     LNode* pB;
-    if(isNULL_withHead(A)){
-        L = NULL;
+    LNode* save = A;
+    if(isNULL_withHead(A))
         return;
-    }else
+    else
         pA = A->next;
-    if(isNULL_withHead(B)){
-        L = NULL;
+    if(isNULL_withHead(B))
         return;
-    }else
+    else
         pB = B->next;
     while(pA != NULL && pB != NULL)
         if(pA->data == pB->data){
-            p->next = (LinkList)malloc(sizeof(LinkList));
-            p = p->next;
-            p->data = pA->data;
+            save->next = pA;
+            save = pA;
             pA = pA->next;
             pB = pB->next;
-        }else if (pA->data < pB->data)
-            pA = pA->next;
+        }else if(pA->data > pB->data)
+            pB = pB->next;
         else
-            pB = pB->next;
-    
-    p->next = NULL;
+            pA = pA->next;
+    save->next = NULL;
 }
 
 int main(void){
     Elemtype testA[] = {1,2,3,8,10,15};
     Elemtype testB[] = {0,1,5,8,9,10,12,13,14};
-    LinkList L;
     LinkList A = InitList_withHead(testA, sizeof(testA) / sizeof(Elemtype));
     LinkList B = InitList_withHead(testB, sizeof(testB) / sizeof(Elemtype));
-    merge_2ascList_common1List(L, A, B);
-    printList_withHead(L);
+    merge_2ascList_2A(A, B);
     printList_withHead(A);
     printList_withHead(B);
     return 0;
